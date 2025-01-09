277e6eeec268b8686adffa4453e49f906adf8bdd
codewithsarthak82 <sarthakdhiman815@gmail.com>
2025-01-09 14:37:41 +0530

    03_client.py

--- 03_client.py
+++ 03_client.py
@@ -5,13 +5,7 @@ client = OpenAI(
     api_key="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"
 )
 command = '''
-[10:46 pm, 8/1/2025] Ruchi Di 💗: Kl kr lio try
-[10:46 pm, 8/1/2025] Ruchi Di 💗: Pese h agar account m toh server issue hoga
-[10:46 pm, 8/1/2025] Sarthak Dhiman: Okay ! mera project almost done ho rakha hai bss iski wajah se ruka hua hai ! 
-[10:47 pm, 8/1/2025] Sarthak Dhiman: Nahi ye patani mastercard nhi leta maine yt video dekhin
-[10:55 pm, 8/1/2025] Ruchi Di 💗: Usi m dalna
-[10:55 pm, 8/1/2025] Sarthak Dhiman: Billing address kya dalun ?
-[10:56 pm, 8/1/2025] Ruchi Di 💗: Gihm
+Enter some samples from your chap from the terminal after running the 01_main_bot.py file.
 '''
 # Create a chat completion request
 completion = client.chat.completions.create(
